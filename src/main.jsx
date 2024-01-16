5f63db6f4c23f5bc2f0ec713558ba437f6558142
Orchizakia00 <zakiaislam290@gmail.com>
2024-01-16 12:24:56 +0600

    project setup completed

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import MainLayout from './layout/MainLayout.jsx';
+import Home from './pages/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
